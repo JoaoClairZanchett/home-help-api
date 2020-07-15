657b6b8808ea7d93d8634b05934127ce3b413d99
JoaoClairZanchett <jcl.junior@hotmail.com>
2020-07-15 20:52:00 -0300

    initial configuration

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,65 @@
+import { Request, Response } from 'express';
+import knex from '../database/connection';
+import UsersService from '../services/UserService';
+class UsersController {
+    
+    usersService = new UsersService();
+    
+    create = async (request: Request, response: Response) => {
+        
+        const {
+            name,
+            email,
+            password
+        } = request.body;
+
+        const users = await knex('users')
+            .where('email', String(email))
+            .distinct()
+            .select('users.*')
+
+        if(users.length > 0) {
+            return response.status(404).json({ message: 'E-mail já cadastrado'});
+        }
+
+        const pass = this.usersService.cryptographyPassword(password);
+
+        const user = {
+            name,
+            email,
+            password: pass,
+        }
+    
+        await knex('users').insert(user);
+    
+        return response.json({ 
+            user
+        })
+    }
+
+    show = async (request: Request, response: Response) => {
+        const { id } = request.params;
+
+        const user = await knex('users').where('id', id).first();
+
+        if(!user) {
+            return response.status(404).json({ message: 'User not found'});
+        }   
+
+        return response.json({user});
+    }
+
+    search = async (request: Request, response: Response) => {
+        const { email } = request.body;
+
+        const users = await knex('users')
+            .where('email', String(email))
+            .distinct()
+            .select('users.*')
+        
+        return response.json(users);
+    }
+
+}
+
+export default UsersController;
---
+++ src/models/UserModel.ts
@@ -0,0 +1,14 @@
+class User {
+    name: string;
+    email: string;
+    password: string;
+    id: number;
+
+    constructor(name: string, email: string, password: string, id: number) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.id = id;
+    }
+    
+}
